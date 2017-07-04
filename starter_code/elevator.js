aad241626e5b525742529d70c7d28b704248f005
Marc Palatsí <mpalatsi@me.com>
2017-07-04 09:41:15 +0200

    done until iteration 4

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,66 @@
+/* jshint esversion:6 */
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.direction = "up";
+    this.waitingList = [];
+    this.passengers = [];
+    this.requests = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    let intervalId = setInterval(() => {
+      this.update();
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(intervalId);
+  }
+
+  update() {
+    this.log();
+  }
+
+checkWaitingList (){
+    let passengersIn = this.waitingList.originFloor.filter(this.floor);
+    _passengersEnter(passengersIn);
+
+}
+
+  _passengersEnter(passenger) {
+    this.passengers.push(passenger);
+    this.requests.push(passenger.destinationFloor);
+  }
+
+  _passengersLeave() {}
+
+  floorUp() {
+    if (this.floor < this.MAXFLOOR) {
+      this.floor++;
+    } else {
+      this.floor = this.floor;
+    }
+  }
+
+  floorDown() {
+    if (this.floor > 0) {
+      this.floor--;
+    } else {
+      this.floor = this.floor;
+    }
+  }
+
+  call(newPerson) {
+    this.requests.push(newPerson.originFloor);
+    this.waitingList.push(newPerson);
+
+  }
+
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,16 @@
+/* jshint esversion:6 */
+
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+let elevator = new Elevator();
+let  marc = new Person("Marc",1,7);
+let  anton = new Person("Anton",0,3);
+let  luis = new Person("Luis",4,0);
+let  inma = new Person("Inma",0,10);
+let  Gloria = new Person("Gloria",8,4);
+
+
+elevator.start();
+elevator.call(marc);
+elevator.checkWaitingList();
